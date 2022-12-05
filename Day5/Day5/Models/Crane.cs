959c9061e3757e298dd7650f3dcce645e8e7f411
SergiuSerb@Nagarro <sergiu.serb@nagarro.com>
2022-12-05 13:57:59 +0200

    Initial solution.

---
+++ Day5/Day5/Models/Crane.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day5.Models
+{
+    public abstract class Crane
+    {
+        public IList<CrateStack> Stacks { get; }
+
+        public Crane(IList<CrateStack> stacks)
+        {
+            Stacks = stacks;
+        }
+
+        protected abstract void Move(int sourceStackId, int destinationStackId, int numberOfCratesToMove);
+
+        public abstract void Move(Move move);
+    }
+}
---
+++ Day5/Day5/Models/Move.cs
@@ -0,0 +1,21 @@
+﻿namespace Day5.Models
+{
+    public class Move
+    {
+        public int Id { get; }
+
+        public int SourceStackId { get; }
+
+        public int DestinationStackId { get; }
+
+        public int NumberOfCratesToMove { get; }
+
+        public Move(int id, int sourceStackId, int destinationStackId, int numberOfCratesToMove)
+        {
+            Id = id;
+            SourceStackId = sourceStackId;
+            DestinationStackId = destinationStackId;
+            NumberOfCratesToMove = numberOfCratesToMove;
+        }
+    }
+}
