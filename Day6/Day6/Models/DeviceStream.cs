cde61b7e8c55d100998a7b3bbac416b87bac48aa
SergiuSerb@Nagarro <sergiu.serb@nagarro.com>
2022-12-06 10:48:19 +0200

    Initial solution.

---
+++ Day6/Day6/Models/DeviceStream.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day6.Models
+{
+    public class DeviceStream
+    {
+        public IList<char> Stream { get; }
+
+        public Queue<char> StartPacketMarker { get; set; }
+        
+        public Queue<char> StartMessageMarker { get; set; }
+
+        public int StartPacketMarkerIndex { get; set; }
+        
+        public int StartMessageMarkerIndex { get; set; }
+
+        public DeviceStream(IList<char> stream)
+        {
+            Stream = stream ?? throw new ArgumentNullException(nameof(stream));
+            DetermineStartPacketMarker();
+            DetermineStartMessageMarker();
+        }
+
+        private void DetermineStartPacketMarker()
+        {
+            StartPacketMarker = new Queue<char>();
+
+            int currentIndex = 0;
+            foreach (char character in Stream)
+            {
+                if (StartPacketMarker.Distinct().Count() == 4)
+                {
+                    StartPacketMarkerIndex = currentIndex;
+                    break;
+                }
+
+                if (StartPacketMarker.Count == 4)
+                {
+                    StartPacketMarker.Dequeue();
+                }
+
+                StartPacketMarker.Enqueue(character);
+                currentIndex++;
+            }
+        }
+        
+        private void DetermineStartMessageMarker()
+        {
+            StartMessageMarker = new Queue<char>();
+
+            int currentIndex = 0;
+            foreach (char character in Stream)
+            {
+                if (StartMessageMarker.Distinct().Count() == 14)
+                {
+                    StartMessageMarkerIndex = currentIndex;
+                    break;
+                }
+
+                if (StartMessageMarker.Count == 14)
+                {
+                    StartMessageMarker.Dequeue();
+                }
+
+                StartMessageMarker.Enqueue(character);
+                currentIndex++;
+            }
+        }
+    }
+}
---
+++ Day6/Day6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Day6.Models;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputLines = ReadInputFile();
+            DeviceStream stream = new DeviceStream(inputLines[0].ToList());
+            DetermineStartOfPackets(stream);
+            DetermineStartOfMessage(stream);
+        }
+
+        private static void DetermineStartOfMessage(DeviceStream stream)
+        {
+            Console.WriteLine($"The start of message is at {stream.StartMessageMarkerIndex}.");
+        }
+
+        private static void DetermineStartOfPackets(DeviceStream stream)
+        {
+            Console.WriteLine($"The start of packets is at {stream.StartPacketMarkerIndex}.");
+        }
+
+        private static string[] ReadInputFile()
+        {
+            string? executingPath = Path.GetDirectoryName( Assembly.GetEntryAssembly()?.Location );
+            const string inputFileName = "input.txt";
+            string inputPath = $"{executingPath}\\{inputFileName}";
+
+            ReadLines( inputPath, out string[] strings );
+    
+            return strings;
+        }
+
+        private static void ReadLines( string s, out string[] inputLines )
+        {
+            try
+            {
+                inputLines = File.ReadAllLines( s );
+            }
+            catch (Exception)
+            {
+                throw new ArgumentNullException( "No lines could be read from input file." );
+            }
+        }
+    }
+}
